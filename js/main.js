e9da864e4d0dd9eaef4bc0730863bee869e45c56
Luciano Lima <lucianolima122123@gmail.com>
2019-01-11 12:13:15 -0200

    Aside nav bar completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 // JavaScript Document
+function asideShow(){
+	var elSpan = document.getElementsByClassName('aside-span');
+	var elNav = document.getElementsByClassName('nav-item');
+	var elAside = document.getElementById('aside-nav');
+	for (var i = 0; i < elSpan.length; i++){
+		elSpan[i].style.visibility = "visible";
+		elNav[i].style.width = "150px";
+		elAside.style.width = "160px";
+	}
+}
+function asideHidde(){
+	var elSpan = document.getElementsByClassName('aside-span');
+	var elNav = document.getElementsByClassName('nav-item');
+	var elAside = document.getElementById('aside-nav');
+	for (var i = 0; i < elSpan.length; i++){
+		elSpan[i].style.visibility = "hidden";
+		elNav[i].style.width = "50px";
+		elAside.style.width = "60px";
+	}
+}
+
+var elAside = document.getElementById('aside-nav');
+elAside.onmouseover = asideShow;
+elAside.onmouseout = asideHidde;
