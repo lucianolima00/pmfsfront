ff396df73f6f833ac370f52d5cd5aa9a8ad11dd4
Luciano Lima <lucianolima122123@gmail.com>
2019-01-11 18:05:44 -0200

    Navbar updated #2

--- js/main.js
+++ js/main.js
@@ -3,43 +3,27 @@ function asideShow(){
 	var elSpan = document.getElementsByClassName('aside-span');
 	var elNav = document.getElementsByClassName('nav-item');
 	var elAside = document.getElementById('aside-nav');
+	var elContent = document.getElementsByClassName('container-fluid');
 	for (var i = 0; i < elSpan.length; i++){
 		elSpan[i].style.visibility = "visible";
 		elNav[i].style.width = "150px";
 		elAside.style.width = "160px";
 	}
+	elContent[0].style.paddingLeft = "160px";
 }
 function asideHidde(){
 	var elSpan = document.getElementsByClassName('aside-span');
 	var elNav = document.getElementsByClassName('nav-item');
 	var elAside = document.getElementById('aside-nav');
+	var elContent = document.getElementsByClassName('container-fluid');
 	for (var i = 0; i < elSpan.length; i++){
 		elSpan[i].style.visibility = "hidden";
-		elNav[i].style.width = "50px";
+		elNav[i].style.width = "47.5px";
 		elAside.style.width = "60px";
 	}
+	elContent[0].style.paddingLeft = "60px";
 }
 
 var elAside = document.getElementById('aside-nav');
 elAside.onmouseover = asideShow;
 elAside.onmouseout = asideHidde;
-
-window.onscroll = function() {stickNavMenu()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-var asidebar = document.getElementById("aside-nav");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function stickNavMenu() {
-	if (window.pageYOffset >= sticky) {
-	  navbar.classList.add("sticky")
-	  asidebar.classList.add("stickyaside")
-	} else {
-	  navbar.classList.remove("sticky");
-	  asidebar.classList.add("stickyaside")
-	}
-}
