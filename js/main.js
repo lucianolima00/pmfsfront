c005741abb8f3f2af865af8b7c007e56d0b92c48
Luciano Lima <lucianolima122123@gmail.com>
2019-01-12 09:03:31 -0200

    Asidebar e Navbar finalizados

--- js/main.js
+++ js/main.js
@@ -1,27 +1,32 @@
-// JavaScript Document
+// Show aside text
 function asideShow(){
 	var elSpan = document.getElementsByClassName('aside-span');
 	var elNav = document.getElementsByClassName('nav-item');
 	var elAside = document.getElementById('aside-nav');
 	var elContent = document.getElementsByClassName('container-fluid');
+	var elFooter = document.getElementsByClassName('footer');
 	for (var i = 0; i < elSpan.length; i++){
 		elSpan[i].style.visibility = "visible";
 		elNav[i].style.width = "150px";
 		elAside.style.width = "160px";
 	}
 	elContent[0].style.paddingLeft = "160px";
+	elFooter[0].style.paddingLeft = "160px";
 }
+//Hidde aside text
 function asideHidde(){
 	var elSpan = document.getElementsByClassName('aside-span');
 	var elNav = document.getElementsByClassName('nav-item');
 	var elAside = document.getElementById('aside-nav');
 	var elContent = document.getElementsByClassName('container-fluid');
+	var elFooter = document.getElementsByClassName('footer');
 	for (var i = 0; i < elSpan.length; i++){
 		elSpan[i].style.visibility = "hidden";
 		elNav[i].style.width = "47.5px";
 		elAside.style.width = "60px";
 	}
 	elContent[0].style.paddingLeft = "60px";
+	elFooter[0].style.paddingLeft = "60px";
 }
 
 var elAside = document.getElementById('aside-nav');
