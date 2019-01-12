ea2306edb5db2524980650fc382c6337f8e91459
Luciano Lima <lucianolima122123@gmail.com>
2019-01-12 12:17:43 -0200

    Final template Site IFG

--- js/main.js
+++ js/main.js
@@ -3,30 +3,32 @@ function asideShow(){
 	var elSpan = document.getElementsByClassName('aside-span');
 	var elNav = document.getElementsByClassName('nav-item');
 	var elAside = document.getElementById('aside-nav');
-	var elContent = document.getElementsByClassName('container-fluid');
+	var elContent = document.getElementsByClassName('container');
 	var elFooter = document.getElementsByClassName('footer');
 	for (var i = 0; i < elSpan.length; i++){
 		elSpan[i].style.visibility = "visible";
 		elNav[i].style.width = "150px";
 		elAside.style.width = "160px";
 	}
-	elContent[0].style.paddingLeft = "160px";
-	elFooter[0].style.paddingLeft = "160px";
+	elContent[0].style.marginLeft = "160px";
+	elContent[0].style.paddingRight = "160px";
+	elFooter[0].style.paddingRight = "200px";
 }
 //Hidde aside text
 function asideHidde(){
 	var elSpan = document.getElementsByClassName('aside-span');
 	var elNav = document.getElementsByClassName('nav-item');
 	var elAside = document.getElementById('aside-nav');
-	var elContent = document.getElementsByClassName('container-fluid');
+	var elContent = document.getElementsByClassName('container');
 	var elFooter = document.getElementsByClassName('footer');
 	for (var i = 0; i < elSpan.length; i++){
 		elSpan[i].style.visibility = "hidden";
 		elNav[i].style.width = "47.5px";
 		elAside.style.width = "60px";
 	}
-	elContent[0].style.paddingLeft = "60px";
-	elFooter[0].style.paddingLeft = "60px";
+	elContent[0].style.marginLeft = "60px";
+	elContent[0].style.paddingRight = "60px";
+	elFooter[0].style.paddingRight = "100px";
 }
 
 var elAside = document.getElementById('aside-nav');
