3c42b5c95aafb3b4e0cf43a9127d633bba3ba4a5
Luciano Lima <lucianolima122123@gmail.com>
2019-01-12 20:54:46 -0200

    Finish template aside full heigth and responsive

--- js/main.js
+++ js/main.js
@@ -11,10 +11,25 @@ function asideShow(){
 		Nav[i].style.width = "150px";
 		Aside.style.width = "160px";
 	}
-	NavBar[0].style.paddingLeft = "100px";
-	Content[0].style.marginLeft = "160px";
-	Content[0].style.paddingRight = "160px";
-	Footer[0].style.paddingRight = "200px";
+	var wid = window.innerWidth;
+	switch(true){
+		case (wid >= 1200):
+			NavBar[0].style.paddingLeft = "160px";
+			Content[0].style.marginLeft = "160px";
+			Content[0].style.paddingRight = "160px";
+			break;
+		case (wid >= 768):
+			NavBar[0].style.paddingLeft = "160px";
+			Content[0].style.marginLeft = "160px";
+			Content[0].style.paddingRight = "160px";
+			break;
+		default:
+			NavBar[0].style.paddingLeft = "60px";
+			Content[0].style.marginLeft = "60px";
+			Content[0].style.paddingRight = "60px";
+			break;
+	}
+
 }
 //Hidde aside text
 function asideHidde(){
@@ -29,10 +44,24 @@ function asideHidde(){
 		Nav[i].style.width = "47.5px";
 		Aside.style.width = "60px";
 	}
-	NavBar[0].style.paddingLeft = "0px";
-	Content[0].style.marginLeft = "60px";
-	Content[0].style.paddingRight = "60px";
-	Footer[0].style.paddingRight = "100px";
+	var wid = window.innerWidth;
+	switch(true){
+		case (wid >= 1200):
+			NavBar[0].style.paddingLeft = "60px";
+			Content[0].style.marginLeft = "60px";
+			Content[0].style.paddingRight = "60px";
+			break;
+		case (wid >= 768):
+			NavBar[0].style.paddingLeft = "60px";
+			Content[0].style.marginLeft = "60px";
+			Content[0].style.paddingRight = "60px";
+			break;
+		default:
+			NavBar[0].style.paddingLeft = "60px";
+			Content[0].style.marginLeft = "60px";
+			Content[0].style.paddingRight = "60px";
+			break;
+	}
 }
 
 var Aside = document.getElementById('aside-nav');
@@ -48,9 +77,11 @@ function toggleDarkLight(){
 	if(currentClass == "dark-mode"){
 		body.className = "light-mode";
 		button.innerHTML = '<i class="fas fa-moon"></i><span class="aside-span">DarkMode</span>';
+		asideShow();
 	}
 	else{
 		body.className = "dark-mode";
 		button.innerHTML = '<i class="fas fa-sun"></i><span class="aside-span">LightMode</span>';
+		asideShow();
 	}
 }
